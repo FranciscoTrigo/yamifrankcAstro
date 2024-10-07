d7098703d9bea40feac4d9ee8a66c3c7faf1d78c
Francisco Trigo <code@trigomijes.com>
2024-10-06 21:32:46 -0500

    removed github link

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,6 @@ import { remarkModifiedTime } from './src/utils/remark-modified-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yamifrankc.com',
-  githubRepoUrl: 'https://github.com/FranciscoTrigo/yamifrankcAstro/blob/main/src/pages/', // Adjust the path as needed
-    integrations: [],
     markdown: {
       remarkPlugins: [remarkModifiedTime],
     },
