b40988b3f6fe26f728d95e8fb809b083fa748d6f
Francisco Trigo <code@trigomijes.com>
2024-10-06 22:03:17 -0500

    edit at guthub link

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { remarkModifiedTime } from './src/utils/remark-modified-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
+  githubRepoUrl: 'https://github.com/FranciscoTrigo/yamifrankcAstro/blob/main/',
     markdown: {
       remarkPlugins: [remarkModifiedTime],
     },
