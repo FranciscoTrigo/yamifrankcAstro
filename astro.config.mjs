7e7a3c7881e967834e0885102659dc1d5eacd922
Francisco Trigo <code@trigomijes.com>
2024-10-09 09:52:34 -0500

    testing cool code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 import { remarkModifiedTime } from './src/utils/remark-modified-time.mjs';
+import mdx from '@astrojs/mdx';
+
+
+import expressiveCode from 'astro-expressive-code';
+
 
 // https://astro.build/config
 export default defineConfig({
-  githubRepoUrl: 'https://github.com/FranciscoTrigo/yamifrankcAstro/blob/main/',
+  integrations: [expressiveCode(), mdx()],
+
     markdown: {
+      syntaxHighlight: 'prism',
       remarkPlugins: [remarkModifiedTime],
     },
   });
-
-  
--- src/content/posts/working-is.md
+++
@@ -1,8 +0,0 @@
----
-title: Working is
-datePublished: 2024-10-08
-dateModified: 
-tags: ["rant"]
----
-
-Working is ranty.
