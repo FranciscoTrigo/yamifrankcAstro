2a1ee1ab2f13dbe29bd27acb49a4d4348d530a21
Francisco Trigo <code@trigomijes.com>
2024-10-09 12:26:37 -0500

    Added srcery theme to code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,20 @@ import { remarkModifiedTime } from './src/utils/remark-modified-time.mjs';
 import mdx from '@astrojs/mdx';
 
 
-import expressiveCode from 'astro-expressive-code';
-import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
+import expressiveCode, { ExpressiveCodeTheme } from 'astro-expressive-code';
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
+import fs from 'node:fs';
+
+const jsoncString = fs.readFileSync(new URL(`src/styles/srcery-code.jsonc`, import.meta.url), 'utf-8')
+const srceryCode = ExpressiveCodeTheme.fromJSONString(jsoncString)
+
+
 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [expressiveCode({
+    themes: [srceryCode],
     plugins: [pluginLineNumbers()],
   }), mdx()],
 
