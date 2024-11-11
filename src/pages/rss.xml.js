03e880735fdebdf4381427d0eb88fa664a04936b
Francisco Trigo <code@trigomijes.com>
2024-11-11 12:27:04 -0500

    rsssss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,35 +3,17 @@ import { getCollection } from 'astro:content';
 
 export async function GET(context) {
   const blog = await getCollection('posts');
-  const rssItems = blog
-  .filter(post => post.data.rss !== false)  // Only include posts with rss: true or undefined
-  .map(post => ({
-    title: post.data.title,
-    pubDate: post.data.datePublished,
-    link: `/posts/${post.slug}/`,
-  }));
-
   return rss({
     title: 'YamiFrankc',
     description: 'A site',
     site: context.site,
-    items: rssItems,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      // Compute RSS link from post `slug`
+      // This example assumes all posts are rendered as `/blog/[slug]` routes
+      link: `/posts/${post.slug}/`,
+    })),
   });
-
-
-
-
-//   return rss({
-//     title: 'YamiFrankc',
-//     description: 'A site',
-//     site: context.site,
-//     items: blog.map((post) => ({
-//       title: post.data.title,
-//       pubDate: post.data.pubDate,
-//       description: post.data.description,
-//       // Compute RSS link from post `slug`
-//       // This example assumes all posts are rendered as `/blog/[slug]` routes
-//       link: `/posts/${post.slug}/`,
-//     })),
-//   });
 }
