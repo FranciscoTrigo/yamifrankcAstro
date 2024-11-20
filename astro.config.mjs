dbac44ab54dc839b2b50081ccda8a6f178abb7ad
Francisco Trigo <code@trigomijes.com>
2024-11-20 11:02:12 -0500

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import { remarkModifiedTime } from './src/utils/remark-modified-time.mjs';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 
 import expressiveCode, { ExpressiveCodeTheme } from 'astro-expressive-code';
 import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
 import fs from 'node:fs';
+
+import sitemap from '@astrojs/sitemap';
 
 const jsoncString = fs.readFileSync(new URL(`src/styles/srcery-code.jsonc`, import.meta.url), 'utf-8')
 const srceryCode = ExpressiveCodeTheme.fromJSONString(jsoncString)
@@ -16,11 +19,11 @@ const srceryCode = ExpressiveCodeTheme.fromJSONString(jsoncString)
 // https://astro.build/config
 export default defineConfig({
     site: 'https://yamifrankc.com',
-
-  integrations: [expressiveCode({
+  integrations: [sitemap(),
+    expressiveCode({
     themes: [srceryCode],
     plugins: [pluginLineNumbers()],
-  }), mdx()],
+  }), mdx(), sitemap()],
 
     markdown: {
       syntaxHighlight: 'prism',
